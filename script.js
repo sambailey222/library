b67349428c28eac978aa7079ecec4e0bcef1f141
Sam Bailey <sam.bailey@live.co.uk>
2020-11-30 15:41:51 +0000

    add function to generate new card

--- script.js
+++ script.js
@@ -44,10 +44,45 @@ console.log(myLibrary);
 const library = document.getElementById("library");
 function createCard() {
     const card = document.createElement('div');
-    card.innerHTML = "pep";
-    // card.style.display = "block";
+    card.classList.add("card");
+
+    const deleteBtn = document.createElement("img");
+    deleteBtn.classList.add("trash");
+    deleteBtn.src = "delete-icon.png";
+    card.appendChild(deleteBtn);
+
+    const cover = document.createElement("img");
+    cover.classList.add("cover");
+    // sort out the logic for book cover default etc later
+    cover.src = "https://images-na.ssl-images-amazon.com/images/I/51uYlDqoIyL._SX326_BO1,204,203,200_.jpg";
+    card.appendChild(cover);
+
+    const bookTitle = document.createElement("h3");
+    bookTitle.classList.add("bookTitle");
+    bookTitle.innerHTML = "Title";
+    card.appendChild(bookTitle);
+
+    const author = document.createElement("h4");
+    author.innerHTML = "Author";
+    card.appendChild(author);
+
+    const pages = document.createElement("h4");
+    pages.innerHTML = "Pages";
+    card.appendChild(pages);
+
+    const readOrNot = document.createElement("h4");
+    readOrNot.classList.add("readText");
+    readOrNot.innerHTML = "Read";
+    card.appendChild(readOrNot);
+
+    const readIcon = document.createElement("img");
+    readIcon.classList.add("readCheck");
+    readIcon.src = "tick-icon.png";
+    card.appendChild(readIcon);
+    
+    // add all of the above to library div
     library.appendChild(card);
 }
 
-// createCard();
+createCard();
 
