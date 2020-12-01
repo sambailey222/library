137de3baf7125430e31d58aebd60345b2b5b81fd
Sam Bailey <sam.bailey@live.co.uk>
2020-12-01 11:57:57 +0000

    add modal book submit form

--- script.js
+++ script.js
@@ -2,48 +2,71 @@ let myLibrary = [
     {title: "Laird of the Rungs", author: "Token", pages: "111", read: "true"}
 ];
 
-
-
-function Book(title, author, pages, read) {
+function Book(title, author, cover, pages, read) {
     this.title = title;
     this.author = author;
+    this.cover = cover;
     this.pages = pages;
     this.read = read;
 }
 
-Book.prototype.printTitle = function() {
-    console.log(this.title)
-}
-console.log(Book.prototype)
+// Book.prototype.printTitle = function() {
+//     console.log(this.title)
+// }
+// console.log(Book.prototype)
 
-function Movie(title) {
-    this.title = title;
-}
+// function Movie(title) {
+//     this.title = title;
+// }
 
-Movie.prototype = Object.create(Book.prototype);
-console.log(Movie.prototype)
+// Movie.prototype = Object.create(Book.prototype);
+// console.log(Movie.prototype)
 
-const LOTR = new Book("LOTR", "JRR", 300, true);
-LOTR.printTitle();
+// const LOTR = new Book("LOTR", "JRR", 300, true);
+// LOTR.printTitle();
 
-const fellowship = new Movie("fellowship");
-fellowship.printTitle();
+// const fellowship = new Movie("fellowship");
+// fellowship.printTitle();
+
+
 
 function addBookToLibrary() {
 // take user's input as new book object
-let title = prompt("Name of book:", "e.g. LOTR");
-let author = prompt("Name of author:", "E.g. Tokenz");
-let pages = prompt("Pages read:", "56");
-let read = prompt("Finished the book?", "true or false");
+console.log("sweet child");
+let title = document.getElementById("title").value;
+let author = document.getElementById("author").value;
+let cover = document.getElementById("cover").value;
+let pages = document.getElementById("pages").value;
+let read = document.getElementById("read").value;
 // create object from responses:
-let newBook = new Book(title, author, pages, read);
+let newBook = new Book(title, author, cover, pages, read);
 console.log(newBook);
 // add new book object to array
 myLibrary.push(newBook);
 console.log(myLibrary);
+createCard(newBook);
+modal.style.display = "none";
 }
 
-addBookToLibrary();
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", () => addBookToLibrary);
+
+// function addBookToLibrary() {
+//     // take user's input as new book object
+//     let title = prompt("Name of book:", "e.g. LOTR");
+//     let author = prompt("Name of author:", "E.g. Tokenz");
+//     let cover = prompt("Add cover art link")
+//     let pages = prompt("Pages read:", "56");
+//     let read = prompt("Finished the book?", "true or false");
+//     // create object from responses:
+//     let newBook = new Book(title, author, cover, pages, read);
+//     console.log(newBook);
+//     // add new book object to array
+//     myLibrary.push(newBook);
+//     console.log(myLibrary);
+//     }
+
+// addBookToLibrary();
 
 const library = document.getElementById("library");
 function createCard(book) {
@@ -58,7 +81,11 @@ function createCard(book) {
     const cover = document.createElement("img");
     cover.classList.add("cover");
     // sort out the logic for book cover default etc later
+    if (!(book.cover)) {
     cover.src = "https://images-na.ssl-images-amazon.com/images/I/51uYlDqoIyL._SX326_BO1,204,203,200_.jpg";
+    } else {
+        cover.src = book.cover;
+    }
     card.appendChild(cover);
 
     const bookTitle = document.createElement("h3");
@@ -96,3 +123,29 @@ function displayBooks(arr) {
     }
 }
 displayBooks(myLibrary);
+
+// Get the modal
+var modal = document.getElementById("bookForm");
+
+// Get the button that opens the modal
+var btn = document.getElementById("newBook");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
